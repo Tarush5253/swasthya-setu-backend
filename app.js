08daf54d593703feda5f3b4b5acd1ede21d876c5
tarush <tarushruhela1234@gmail.com>
2025-06-13 23:03:52 +0530

    add cors

--- app.js
+++ app.js
@@ -16,7 +16,14 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://swasthya-setu-nu.vercel.app',
+    'http://localhost:3000' // For local development
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
